bc11f79283fdf7ee835297ccfb75a3bde8a85750
Griffin Steffy <steffygm@rose-hulman.edu>
2019-01-11 05:51:46 -0800

    it is pretty now

--- benchmarks/CP.sh
+++ benchmarks/CP.sh
@@ -2,7 +2,7 @@
 echo "Running CP Benchmark"
 cd ../
 repo=$(pwd)
-cd test/
+cd ./benchmarks/test
 ~/ispass2009-benchmarks/bin/release/CP > $repo/output/CP.OUT
 echo "CP Complete"
 echo "Exiting..."
--- benchmarks/RAY.sh
+++ benchmarks/RAY.sh
@@ -7,7 +7,7 @@ fi
 echo "Running RAY $1 $2 Benchmark"
 cd ../
 repo=$(pwd)
-cd test/
+cd ./benchmarks/test
 ~/ispass2009-benchmarks/bin/release/RAY $1 $2 > $repo/output/RAY.OUT
 echo "Ray $1 $2 Complete"
 echo "Exiting..."
